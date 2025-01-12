9f54c702efc4782174f7c5b7618b3fb9ed16523f
mason-e <engelmason@gmail.com>
2025-01-11 20:07:18 -0700

    First two GP acts with correct blocks

--- CadEditor/game_settings/1-Green-2.cs
+++ CadEditor/game_settings/1-Green-2.cs
@@ -5,14 +5,11 @@ public class Data
 { 
   public OffsetRec getScreensOffset()  { return new OffsetRec(0x8cc0, 6 , 8*6, 8, 6);   }
   
-  public OffsetRec getVideoOffset()     { return new OffsetRec(0x0 , 1   , 0x1000);  }
-  public OffsetRec getPalOffset  ()     { return new OffsetRec(0x0 , 1   , 16); }
-  public GetVideoChunkFunc    getVideoChunkFunc()    { return Utils.getVideoChunk(new[] {"chr1(a).bin"}); }
-  
   public OffsetRec getBlocksOffset()    { return new OffsetRec(0x8011, 1  , 0x1000);  }
-  public int getBlocksCount()           { return 244; }
-  public int getBigBlocksCount()        { return 244; }
+  public int getBlocksCount()           { return 85; }
+  public int getBigBlocksCount()        { return 85; }
   public int getPalBytesAddr()          { return 0x89e1; }
   
-  public GetPalFunc           getPalFunc()           { return Utils.readPalFromBin(new[] {"pal1(b).bin"}); }
+  public int getPalAddress()            { return 0x117E5; }
+  public int getPatternTableAddress()   { return 0x20010; }
 }
