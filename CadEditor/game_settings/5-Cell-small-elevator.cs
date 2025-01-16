66e6a04f6113dbc5b274950bbbdb0822ac103320
mason-e <engelmason@gmail.com>
2025-01-15 22:33:42 -0700

    Cell added

---
+++ CadEditor/game_settings/5-Cell-small-elevator.cs
@@ -0,0 +1,13 @@
+using CadEditor;
+using System;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0xe03d, 1 , 8*15, 8, 15);   }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xd2d8, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 124; }
+  public int getPalBytesAddr()          { return 0xdaa8; }
+  public int getPalAddress()            { return 0x11971; } 
+  public int[] getPatternTableAddresses()   { return new[] {0x5000, 0x7800}; }
+}
